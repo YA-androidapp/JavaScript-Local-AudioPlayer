07d6587b95651e5be6c14f75a0606263fb2af08c
YA <YA-androidapp@users.noreply.github.com>
2021-12-22 18:13:20 +0900

    Update UI

--- js/app.js
+++ js/app.js
@@ -46,11 +46,7 @@ window.onload = function () {
     });
 };
 
-function trimnullchar(str) {
-    return str.replace(/\0.*$/g, "")
-}
-
-function addTableRow(tableId, items) {
+function addTableRow(tableId, items, file) {
     var tableElem = document.getElementById(tableId);
     let ncol = 4;
     var trElem = document.createElement("tr");
@@ -59,15 +55,27 @@ function addTableRow(tableId, items) {
         tdElem.textContent = items[i];
         trElem.appendChild(tdElem);
     }
+
+    if (file.type.match(/audio\/*/)) {
+        const audioElem = document.createElement('audio');
+        audioElem.controls = true;
+        const reader = new FileReader();
+        reader.onload = (event) => {
+            audioElem.src = event.target.result;
+        }
+        reader.readAsDataURL(file);
+        var tdElem = document.createElement("td");
+        tdElem.appendChild(audioElem);
+        trElem.appendChild(tdElem);
+    }
+
     tableElem.appendChild(trElem);
 }
 
-function addListItem(ulId, item) {
-    var ulElem = document.getElementById(ulId);
-    var liElem = document.createElement("li");
-    liElem.className = "list-group-item";
-    liElem.appendChild(document.createTextNode(item));
-    ulElem.appendChild(liElem);
+function clearFiles() {
+    document.getElementById("files").value = null;
+    document.getElementById("fileNum").innerHTML = "0";
+    document.getElementById("fileSize").innerHTML = "0";
 }
 
 function getId3(file) {
@@ -75,7 +83,7 @@ function getId3(file) {
         if (!data) { return; }
         if (typeof data.header === "undefined"
             || typeof data.frame === "undefined") { return; }
-        addTableRow("playlist-table", [data.frame.artist, data.frame.album, data.frame.track, data.frame.title]);
+        addTableRow("playlist-table", [data.frame.artist, data.frame.album, data.frame.track, data.frame.title], file);
     }).bind(this));
 }
 
@@ -85,6 +93,10 @@ function getId3s(fileList) {
             getId3(file);
         }
     });
+}
+
+function trimnullchar(str) {
+    return str.replace(/\0.*$/g, "")
 }
 
 function updateSize(fileList) {
@@ -97,14 +109,8 @@ function updateSize(fileList) {
     let sOutput = nBytes + " bytes";
     const aMultiples = ["KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
     for (nMultiple = 0, nApprox = nBytes / 1024; nApprox > 1; nApprox /= 1024, nMultiple++) {
-        sOutput = nApprox.toFixed(3) + " " + aMultiples[nMultiple] + " (" + nBytes + " bytes)";
+        sOutput = nApprox.toFixed(3) + " " + aMultiples[nMultiple] + " (" + nBytes.toLocaleString() + " bytes)";
     }
     document.getElementById("fileNum").innerHTML = nFiles;
     document.getElementById("fileSize").innerHTML = sOutput;
-}
-
-function clearFiles() {
-    document.getElementById("files").value = null;
-    document.getElementById("fileNum").innerHTML = "0";
-    document.getElementById("fileSize").innerHTML = "0";
 }
